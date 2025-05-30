fdf7481e23af9675e1509b2b27cda10a2b35dbcc
Ilafer Azizli <ezizliilafer@gmail.com>
2025-05-30 19:09:22 +0400

    NextCommit

--- Mentor/Controllers/GetStartedController.cs
+++ Mentor/Controllers/GetStartedController.cs
@@ -14,50 +14,50 @@ namespace Mentor.Controllers
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Started(StartVm model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+        //[HttpPost]
+        //public async Task<IActionResult> Started(StartVm model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(model);
+        //    }
 
-            var existingUser = await userManager.FindByNameAsync(model.Username);
-            if (existingUser != null)
-            {
-                ModelState.AddModelError(nameof(model.Username), "This username is already taken");
-                return View(model);
-            }
+        //    var existingUser = await userManager.FindByNameAsync(model.Username);
+        //    if (existingUser != null)
+        //    {
+        //        ModelState.AddModelError(nameof(model.Username), "This username is already taken");
+        //        return View(model);
+        //    }
 
-            var newUser = new AppUser
-            {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                UserName = model.Username,
-                Age = model.Age,
-            };
+        //    var newUser = new AppUser
+        //    {
+        //        FirstName = model.FirstName,
+        //        LastName = model.LastName,
+        //        UserName = model.Username,
+        //        Age = model.Age,
+        //    };
 
-            var creationResult = await userManager.CreateAsync(newUser, model.Password);
+        //    var creationResult = await userManager.CreateAsync(newUser, model.Password);
 
-            if (!creationResult.Succeeded)
-            {
-                foreach (var error in creationResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-                return View();
-            }
+        //    if (!creationResult.Succeeded)
+        //    {
+        //        foreach (var error in creationResult.Errors)
+        //        {
+        //            ModelState.AddModelError(string.Empty, error.Description);
+        //        }
+        //        return View();
+        //    }
 
-           // await userManager.AddToRoleAsync(newUser, "Member");
+        //   // await userManager.AddToRoleAsync(newUser, "Member");
 
-            return RedirectToAction("Login", "Getstarted");
-        }
+        //    return RedirectToAction("Login", "Getstarted");
+        //}
 
 
 
-        public IActionResult Login()
-        {
-            return View();
-        }
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
     }
 }
